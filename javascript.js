60cea8190c621df7fc10e3d74b724d5e69beb452
Jeppe Plauborg Pedersen <plauborgmedia@gmail.com>
2026-03-27 20:40:11 +0100

    removed all unused elements

--- javascript.js
+++ javascript.js
@@ -23,8 +23,13 @@ body.appendChild(divRes);
 divRes.appendChild(pScore);
 divRes.appendChild(pRes);
 
+//Scores
+let humanScore = 0;
+let computerScore = 0;
+let rounds = 0;
 
-// Step 2: getComputerChoice
+
+// Her vælger computeren enten sten, saks eller papir med 1/3 change for hver.
 
 function getComputerChoice () {
     let randNum = Math.random();
@@ -38,115 +43,77 @@ function getComputerChoice () {
     }
 }
 
-// Step 3: getHumanChoice
+// Function that desides who win in a round and returns the output and increases score for the winner
+function playRound (humanChoice, computerChoice) {
 
-function getHumanChoice () {
-    let humanPrompt = prompt("Choose Rock, Paper or Scissor", "");
-    humanPrompt = humanPrompt.toLowerCase();
+    //Tied
+    if (humanChoice === "rock" && computerChoice === "rock") {
+        pScore.textContent = `Tie: Both Rock`
+    } else if (humanChoice === "scissor" && computerChoice === "scissor") {
+        pScore.textContent = `Tied: Both Scissor`
+    } else if (humanChoice === "paper" && computerChoice === "paper") {
+        pScore.textContent = `Tied: Both Scissor`
+    } 
+    
+    //Lose
+    else if (humanChoice === "rock" && computerChoice === "paper") {
+        ++computerScore
+        pScore.textContent = `You Lose: Paper beats Rock`
+    } else if (humanChoice === "paper" && computerChoice === "scissor") {
+        ++computerScore
+        pScore.textContent = `You Lose: Scissor beats Paper`
+    } else if (humanChoice === "scissor" && computerChoice === "rock") {
+        ++computerScore
+        pScore.textContent = `You Lose: Rock beats Scissor`
+    } 
+    
+    //Win
+    else if (humanChoice === "rock" && computerChoice === "scissor") {
+        ++humanScore
+        pScore.textContent = `You Win: Rock beats Paper`
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        ++humanScore
+        pScore.textContent = `You Win: Paper beats Rock`
+    } else if (humanChoice === "scissor" && computerChoice === "paper") {
+        ++humanScore
+        pScore.textContent = `You Win: Scissor beats Paper`
 
-    if (humanPrompt === "rock" || humanPrompt === "paper" || humanPrompt === "scissor") {
-        return humanPrompt;
-    } else {
-        return "Wrong Input";
     }
 
-
+    //Default
+    else {
+        return "FUCK MAND LORTET VIRKER IKKEEE!!!!"
+    }
 }
-
-
-
-function playGame () {
-
-    //Scores
-    let humanScore = 0;
-    let computerScore = 0;
-    let rounds = 0;
-
-    // Function that desides who win in a round and returns the output and increases score for the winner
-    function playRound (humanChoice, computerChoice) {
-        console.log(humanChoice);
-        console.log(computerChoice);
-
-
-        //Tied
-        if (humanChoice === "rock" && computerChoice === "rock") {
-            pScore.textContent = `Tie: Both Rock`
-        } else if (humanChoice === "scissor" && computerChoice === "scissor") {
-            pScore.textContent = `Tied: Both Scissor`
-        } else if (humanChoice === "paper" && computerChoice === "paper") {
-            pScore.textContent = `Tied: Both Scissor`
-        } 
-        
-        //Lose
-        else if (humanChoice === "rock" && computerChoice === "paper") {
-            ++computerScore
-            pScore.textContent = `You Lose: Paper beats Rock`
-        } else if (humanChoice === "paper" && computerChoice === "scissor") {
-            ++computerScore
-            pScore.textContent = `You Lose: Scissor beats Paper`
-        } else if (humanChoice === "scissor" && computerChoice === "rock") {
-            ++computerScore
-            pScore.textContent = `You Lose: Rock beats Scissor`
-        } 
-        
-        //Win
-        else if (humanChoice === "rock" && computerChoice === "scissor") {
-            ++humanScore
-            pScore.textContent = `You Win: Rock beats Paper`
-        } else if (humanChoice === "paper" && computerChoice === "rock") {
-            ++humanScore
-            pScore.textContent = `You Win: Paper beats Rock`
-        } else if (humanChoice === "scissor" && computerChoice === "paper") {
-            ++humanScore
-            pScore.textContent = `You Win: Scissor beats Paper`
-
-        }
-
-        //Default
-        else {
-            return "FUCK MAND LORTET VIRKER IKKEEE!!!!"
-        }
-    }
     
 
-    // UI ELEMENTS
+// UI ELEMENTS
 
-    div.addEventListener("click", startGame);
+div.addEventListener("click", startGame);
 
-    function startGame (e) {
-        rounds += 1;
-        console.log(rounds);
-        
-        if (rounds < 5) {
-            playRound(e.target.textContent, getComputerChoice());
+function startGame (e) {
+    rounds += 1;
+    console.log(rounds);
+    
+    if (rounds < 5) {
+        playRound(e.target.textContent, getComputerChoice());
 
-            if (rounds === 4) {
-                if (humanScore > computerScore) {
-                    console.log("You WIN!!!")
-                    pRes.textContent = `You WIN!!! | Score: Human ${humanScore} - Computer ${computerScore}`;
-                } else if (humanScore < computerScore) {
-                    console.log("You LOSE!!")
-                    pRes.textContent = `You LOSE!!! | Score: Human ${humanScore} - Computer ${computerScore}`;
-                } else {
-                    console.log("It's TIED")
-                    pRes.textContent = `It's TIED | Score: Human ${humanScore} - Computer ${computerScore}`;
-                }
+        if (rounds === 4) {
+            if (humanScore > computerScore) {
+                console.log("You WIN!!!")
+                pRes.textContent = `You WIN!!! | Score: Human ${humanScore} - Computer ${computerScore}`;
+            } else if (humanScore < computerScore) {
+                console.log("You LOSE!!")
+                pRes.textContent = `You LOSE!!! | Score: Human ${humanScore} - Computer ${computerScore}`;
+            } else {
+                console.log("It's TIED")
+                pRes.textContent = `It's TIED | Score: Human ${humanScore} - Computer ${computerScore}`;
             }
-            
         }
+        
     }
-    
-    // playRound(getHumanChoice(), getComputerChoice());
-
-    /* Logic that plays 5x
-    for (i = 0; i < 5; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
-    } */
-
-
 }
 
-playGame();
 
 
 
